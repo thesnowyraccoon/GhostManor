8b1f8cea87735f18d6b57b84ebe969b954aff662
Snowy <snowytheraccoon@gmail.com>
2025-07-29 09:25:26 +0200

    Update PlayerController.cs
    Organized and added notes to playercontroller

--- theGayGame/Assets/Scripts/PlayerController.cs
+++ theGayGame/Assets/Scripts/PlayerController.cs
@@ -6,46 +6,53 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // Inputs
     InputAction moveAction;
     InputAction sprintAction;
     InputAction jumpAction;
     InputAction lookAction;
 
+    // GameObjects/Components
+    [Header("Components")]
     [SerializeField] private GameObject playerCamera;
     [SerializeField] private Transform player;
     [SerializeField] private CharacterController controller;
     [SerializeField] private Transform groundCheck;
 
-    float groundDistance = 0.4f;
-
-    public LayerMask groundMask;
-
-    bool isGrounded;
-
-    float xRotation = 0f;
-
-    float currentSpeed;
-
+    // Movement/Look Speeds
+    [Header("Movement Speeds")]
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float sprintSpeed = 10f;
     [SerializeField] private float lookSpeed = 100f;
 
-    [SerializeField] private float jumpHeight = 3f;
+    // Movement Settings
+    float currentSpeed;
+    Vector3 velocity;
 
+    float yRotation = 0f;   // Look rotation
+
+    // Jump Settings
+    [Header("Jump Settings")]
+    [SerializeField] private float jumpHeight = 3f;
     [SerializeField] private float gravity = -9.8f;
 
-    Vector3 velocity;
+    public LayerMask groundMask;    // Collider check for ground
+
+    // Ground Settings
+    float groundDistance = 0.4f;    
+    bool isGrounded;
 
     void Start()
     {
+        // Initialize Inputs
         moveAction = InputSystem.actions.FindAction("Move");
         sprintAction = InputSystem.actions.FindAction("Sprint");
         jumpAction = InputSystem.actions.FindAction("Jump");
         lookAction = InputSystem.actions.FindAction("Look");
 
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;   // Set cursor in game window
 
-        currentSpeed = moveSpeed;
+        currentSpeed = moveSpeed;   // Initializes movement to default speed
     }
 
     void Update()
@@ -57,61 +64,59 @@ public class PlayerController : MonoBehaviour
 
         Sprint();
         Jump();
-        
     }
 
-    void Move()
+    void Move() // Player movement according to Unity Input System
     {
-        Vector2 moveValue = moveAction.ReadValue<Vector2>();
-        Vector3 move = transform.right * moveValue.x + transform.forward * moveValue.y;
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();    // Reads Unity Input System values
+        Vector3 moveDirection = transform.right * moveValue.x + transform.forward * moveValue.y; // Move directions
 
-        // movement code
-        controller.Move(move * currentSpeed * Time.deltaTime);
+        controller.Move(moveDirection * currentSpeed * Time.deltaTime); // Player movement
 
-        velocity.y += gravity * Time.deltaTime;
+        velocity.y += gravity * Time.deltaTime; // Gravity calculations
 
-        controller.Move(velocity * Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime); // Player gravity
     }
 
-    void Sprint()
+    void Sprint()   // Player sprint on input
     {
-        if (sprintAction.IsPressed())
+        if (sprintAction.IsPressed())   // Input check
         {
-            currentSpeed = sprintSpeed;
+            currentSpeed = sprintSpeed; // Increase player speed 
         }
         else
         {
-            currentSpeed = moveSpeed;
+            currentSpeed = moveSpeed;   // Sets to default player speed
         }
     }
 
-    void Jump()
+    void Jump() // Player jump on input
     {
-        if (jumpAction.IsPressed() && isGrounded)
+        if (jumpAction.IsPressed() && isGrounded)   // Input and ground check
         {
             //jump code
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);    // Player jumps and is affected by gravity
         }
     }
 
-    void Look()
+    void Look() // Player look/camera acccording to Unity Input System
     {
-        Vector2 lookValue = lookAction.ReadValue<Vector2>() * lookSpeed * Time.deltaTime;
+        Vector2 lookValue = lookAction.ReadValue<Vector2>() * lookSpeed * Time.deltaTime;   // Reads value from Unity Input System
 
-        xRotation -= lookValue.y;
-        xRotation = Mathf.Clamp(xRotation, -70f, 70f);
+        yRotation -= lookValue.y;
+        yRotation = Mathf.Clamp(yRotation, -70f, 70f);  // Look direction in the y-axis, constrained at angles
 
-        playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        player.Rotate(Vector3.up * lookValue.x);
+        playerCamera.transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f); // Camera movement in the y-axis
+        player.Rotate(Vector3.up * lookValue.x);    // Player and camera movement in the x-axis
     }
 
-    void Ground()
+    void Ground()   // Check for ground
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); // Collision check for ground
 
         if (isGrounded && velocity.y < 0)
         {
-            velocity.y = -2f;
+            velocity.y = -2f;   
         }
     }
 }
