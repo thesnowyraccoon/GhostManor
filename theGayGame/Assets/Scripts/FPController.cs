660461132ce2f6b3ff0bfef2d8d8989a2598eab1
Snowy <snowytheraccoon@gmail.com>
2025-08-08 16:39:01 +0200

    Revert "SampleScene + FPPlayerController - Added pickup and crouch mechanics"
    This reverts commit 67214057b73c7143c37541df36ddb4780ae2ab74.

--- theGayGame/Assets/Scripts/FPController.cs
+++ theGayGame/Assets/Scripts/FPController.cs
@@ -5,31 +5,17 @@ public class FPController : MonoBehaviour
 {
     [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float sprintSpeed = 10f;
     [SerializeField] private float gravity = -9.81f;
     [SerializeField] private float jumpHeight = 1.5f;
-
-    private float currentSpeed;
 
     [Header("Look Settings")]
     [SerializeField] private Transform cameraTransform;
     [SerializeField] private float lookSensitivity = 2f;
     [SerializeField] private float verticalLookLimit = 90f;
 
-    [Header("Crouch Settings")]
-    [SerializeField] float crouchHeight = 1f;
-    [SerializeField] float standHeight = 2f;
-    [SerializeField] float crouchSpeed = 2.5f;
-
-    [Header("Pickup Settings")]
-    [SerializeField] private float pickupRange = 3f;
-    [SerializeField] private Transform holdPoint;
-
-    private PickUpObject heldObject;
-
     [Header("Shooting")]
-    [SerializeField] public GameObject bulletPrefab;
-    [SerializeField] public Transform gunPoint;
+    public GameObject bulletPrefab;
+    public Transform gunPoint;
     [SerializeField] private float bulletSpeed = 1000f;
 
     private CharacterController controller;
@@ -44,19 +30,12 @@ public class FPController : MonoBehaviour
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-
-        currentSpeed = moveSpeed;
     }
 
     private void Update()
     {
         HandleMovement();
         HandleLook();
-
-        if (heldObject != null)
-        {
-            heldObject.MoveToHoldPoint(holdPoint.position);
-        }
     }
 
     public void OnMovement(InputAction.CallbackContext context)
@@ -72,7 +51,7 @@ public class FPController : MonoBehaviour
     public void HandleMovement()
     {
         Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
-        controller.Move(move * currentSpeed * Time.deltaTime);
+        controller.Move(move * moveSpeed * Time.deltaTime);
 
         if (controller.isGrounded && velocity.y < 0)
             velocity.y = -2f;
@@ -100,55 +79,15 @@ public class FPController : MonoBehaviour
         }
     }
 
-    public void OnCrouch(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            controller.height = crouchHeight;
-            currentSpeed = crouchSpeed;
-        }
-        else if (context.canceled)
-        {
-            controller.height = standHeight;
-            currentSpeed = moveSpeed;
-        }
-    }
-
     public void OnSprint(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            currentSpeed = sprintSpeed;
+            moveSpeed = 10f;
         }
         else
         {
-            currentSpeed = moveSpeed;
-        }
-    }
-
-    public void OnPickUp(InputAction.CallbackContext context)
-    {
-        if (!context.performed) return;
-
-        if (heldObject == null)
-        {
-            Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
-
-            if (Physics.Raycast(ray, out RaycastHit hit, pickupRange))
-            {
-                PickUpObject pickUp = hit.collider.GetComponent<PickUpObject>();
-
-                if (pickUp != null)
-                {
-                    pickUp.PickUp(holdPoint);
-                    heldObject = pickUp;
-                }
-            }
-        }
-        else
-        {
-            heldObject.Drop();
-            heldObject = null;
+            moveSpeed = 5f;
         }
     }
 
