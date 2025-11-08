4baebe7e27880c93a5cb5731966c147203959e3c
SkullManea <venturousskull@gmail.com>
2025-11-08 19:05:34 +0200

    Attempt to fix lighting and AI script

--- GhostManor/Assets/Scripts/NPC/AIPatrol.cs
+++ GhostManor/Assets/Scripts/NPC/AIPatrol.cs
@@ -6,25 +6,43 @@ public class AIPatrol : MonoBehaviour
     private NavMeshAgent NPC;
     public Transform[] patrolPoints;
     private int currentPoint = 0;
+
+     [SerializeField]
+     private Animator idle;
+
+    private bool isMoving = true;
+
     void Start()
     {
+        idle = GetComponentInParent<Animator>();
         NPC = GetComponent<NavMeshAgent>();
     }
 
     
     void Update()
     {
-        if (!NPC.pathPending&& NPC.remainingDistance < 0.5f)
+        // if dialogueisactive
+        // {
+        // NPC.acceleration = 0f;
+        //idle.SetBool("isMoving", isMoving); 
+        // }
+
+        // else
+        if (!NPC.pathPending && NPC.remainingDistance < 0.5f)
         {
             currentPoint++;
 
             //Reset to the first point
             if (currentPoint >= patrolPoints.Length)
             {
+                //NPC.acceleration = 120f;
+                idle.SetBool("isMoving", isMoving); 
                 currentPoint = 0;
             }
 
             NPC.SetDestination(patrolPoints[currentPoint].position);
         }
+
+        
     }
 }
